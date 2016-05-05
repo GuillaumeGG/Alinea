2fea8b4951ec0c754a75638404bd7ccdb8ef1cc3
Guillaume.Grosshenny <guillaume.grosshenny@etu.unistra.fr>
2016-05-05 18:31:13 +0200

    FIX : transpose

--- operations.c
+++ operations.c
@@ -7,45 +7,45 @@
 
 Matrix addition(Matrix m, Matrix n){
 
-	if(m->ncols != n->ncols && m->nrows != n->ncols){
-		fprintf(stderr,"matrices de tailles différentes\n");
-		return m;
-	}
-	int i,j;
-	double s;
-	Matrix M = newMatrix(m->nrows, m->ncols);
-	for(i=0; i<m->nrows; i++){
-		for(j=0; j<m->ncols; j++){
-				s=getElt(m,i,j) + getElt(n,i,j);
-				setElt(M, i, j, s);
-		}
-	}
-	return M;
+  if(m->ncols != n->ncols && m->nrows != n->ncols){
+    fprintf(stderr,"matrices de tailles différentes\n");
+    return m;
+  }
+  int i,j;
+  double s;
+  Matrix M = newMatrix(m->nrows, m->ncols);
+  for(i=0; i<m->nrows; i++){
+    for(j=0; j<m->ncols; j++){
+        s=getElt(m,i,j) + getElt(n,i,j);
+        setElt(M, i, j, s);
+    }
+  }
+  return M;
 }
 
 Matrix multiplication(Matrix m, Matrix n){
-	int i,j,k;
-	E s;
-	Matrix M = newMatrix(m->nrows, m->ncols);
-	for(i=0; i<m->nrows; i++){
-		for(j=0; j<m->ncols; j++){
-			setElt(M, i, j, 0);
-			for(k=0; k<n->ncols; k++){
-				s = getElt(M,i,j)+(getElt(m,i,k)*getElt(n,k,j));
-				setElt(M, i, j, s);				
-			}
-		}
-	}
-	return M;
+  int i,j,k;
+  E s;
+  Matrix M = newMatrix(m->nrows, m->ncols);
+  for(i=0; i<m->nrows; i++){
+    for(j=0; j<m->ncols; j++){
+      setElt(M, i, j, 0);
+      for(k=0; k<n->ncols; k++){
+        s = getElt(M,i,j)+(getElt(m,i,k)*getElt(n,k,j));
+        setElt(M, i, j, s);       
+      }
+    }
+  }
+  return M;
 }
 
 Matrix transpose(Matrix m){
-	int i,j;
-	Matrix M = newMatrix(m->ncols, m->nrows);
-	for(i=0; i<M->nrows; i++){
-		for(j=0; j<M->ncols; j++){
-			setElt(M, j, i, getElt(m, i, j));
-		}
-	}
-	return M;
+  int i,j;
+  Matrix M = newMatrix(m->ncols, m->nrows);
+  for(i=0; i<m->nrows; i++){
+    for(j=0; j<m->ncols; j++){
+      setElt(M, j, i, getElt(m, i, j));
+    }
+  }
+  return M;
 }
